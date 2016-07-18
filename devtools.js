510ffe9adc34abf0d9da55c1a12631312e9828dc
zxc0328 <120797887@qq.com>
2016-07-18 14:51:35 +0800

    add inspect node method

--- devtools.js
+++ devtools.js
@@ -22,7 +22,34 @@ var devtools = new devtoolsView({
             data: {}
         },
     },
+    onInspectNode: function() {
+        var uuid = this.data.currentNode.uuid;
+        console.log("inspect(window.__RDGH__.filter(function(node) { return node.uuid === '" + uuid + "'})[0].node)")
+        chrome.devtools.inspectedWindow.eval(
+            "inspect(window.__RDGH__.filter(function(node) { return node.uuid === '" + uuid + "'})[0].node)",
+            function(result, isException) {
+                console.log("on ins!!", result, isException)
+            }
+        );
+    }
 }).$inject("#devtoolsInject")
+
+var findElementByUuid = function(nodes, uuid) {
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].uuid === uuid) {
+            return nodes[i]
+        } else {
+            if (nodes[i].childNodes.length) {
+                var result = findElementByUuid(nodes[i].childNodes, uuid);
+                if (result) {
+                    return result;
+                }
+            }
+        }
+    }
+}
+
+
 
 devtools.$on("initNodes", function(nodes) {
     this.data.nodes = nodes;
@@ -37,7 +64,15 @@ devtools.$on("clickElement", function(node) {
 
 devtools.$on("dataUpdate", function(nodes) {
     this.data.nodes = nodes;
-    this.$update();
+    var currNode = findElementByUuid(nodes, this.data.currentNode.uuid)
+    if (currNode) {
+        this.data.currentNode = currNode;
+        this.$update();
+    } else {
+        this.data.currentNode = null;
+        this.data.currentNode = this.data.nodes[0];
+        this.$update();
+    }
 })
 
 devtools.$on("reRender", function(nodes) {
@@ -47,14 +82,13 @@ devtools.$on("reRender", function(nodes) {
     this.$update();
 })
 
-
-backgroundPageConnection.onMessage.addListener(function (message) {
+backgroundPageConnection.onMessage.addListener(function(message) {
     console.log("devtools received message", message)
     if (message.type === "dataUpdate") {
+        devtools.$emit("dataUpdate", message.nodes);
+    } else if (message.type === "reRender") {
         devtools.$emit("reRender", message.nodes);
-    }else if (message.type === "reRender") {
-        devtools.$emit("reRender", message.nodes);
-    }else if (message.type === "initNodes") {
+    } else if (message.type === "initNodes") {
         devtools.$emit("initNodes", message.nodes);
     }
 });
--- frontend/inject.js
+++ frontend/inject.js
@@ -5,17 +5,49 @@ var devtoolsModel = (function() {
     var treeWalker = function(parentNode, children) {
         for (var i = 0; i < children.length; i++) {
             var node = {
+                uuid: children[i].uuid,
                 name: children[i].name || "node",
                 data: children[i].data,
                 childNodes: []
             }
             parentNode.childNodes.push(node);
             if (children[i]._children) {
-                treeWalker(children[i], children[i]._children);
+                treeWalker(node, children[i]._children);
             }
         }
     }
+
+    var guid = function() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+    var uuidGen = function(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (!arr[i].uuid) {
+                arr[i].uuid = guid();
+            }
+        }
+    }
+    
+    // generate uuid for the first time
+    uuidGen(ins);
     return {
+
+        sanitize: function(store) {
+            var str = JSON.stringify(store, function(key, value) {
+                if (value instanceof Regular) {
+                    return;
+                }
+                return value;
+            });
+            return JSON.parse(str);
+        },
         get: function(index) {
             if (index) {
                 return ins[index];
@@ -25,16 +57,21 @@ var devtoolsModel = (function() {
         storeGen: function() {
             store = [];
             for (var i = 0; i < ins.length; i++) {
-                if (ins[i].parentNode) {
+                if (ins[i].$root === ins[i]) {
                     var node = {
+                        uuid: ins[i].uuid,
                         name: "root",
                         data: ins[i].data,
                         childNodes: []
                     }
                     store.push(node);
-                    treeWalker(node, ins[i]._children)
+                    if (ins[i]._children.length) {
+                        console.log(node, ins[i]._children)
+                        treeWalker(node, ins[i]._children)
+                    }
                 }
             }
+            store = this.sanitize(store)
             return store;
         },
         getStore: function() {
@@ -59,6 +96,7 @@ var devtoolsModel = (function() {
                 ins[i].$off("$destory");
                 ins[i].$on("update", function() {
                     if (self.ifChanged()) {
+                        uuidGen(ins)
                         self.addEvent();
                         window.postMessage({
                             type: "FROM_PAGE",
@@ -68,7 +106,6 @@ var devtoolsModel = (function() {
                             }
                         }, "*");
                     } else {
-                        console.log(self)
                         window.postMessage({
                             type: "FROM_PAGE",
                             data: {
@@ -80,7 +117,6 @@ var devtoolsModel = (function() {
                 });
 
                 ins[i].$on("$destroy", function() {
-                    console.log('on des!!', this.name)
                     self.remove(this);
                 });
             }
