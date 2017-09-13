2302f033cc96978ffab0f5aa85f652c9e8e5da8b
MO <fengzilong1992@gmail.com>
2017-09-12 21:06:38 -0500

    Fix typo

--- src/devtools/components/Tabs.js
+++ src/devtools/components/Tabs.js
@@ -8,7 +8,7 @@ const Tabs = Regular.extend({
                     {#list source as s}
                         {#if s.key === "data" }
                           <div class="mdl-tooltip roboto" data-mdl-for="ttl2">
-                            the data displayed here is passed from inspected page by IPC, which means only JSON-ifiable object is allowed. If you want to inspect DOM or function in data, please using $r in console
+                            the data displayed here is passed from inspected page by IPC, which means only JSON-ifiable object is allowed. If you want to inspect DOM or function in data, please use $r in console
                           </div>
                           <div r-md=""  class="tabs-header-item" id="ttl2" on-click="{ this.onTabClick( s.key ) }">
                           { s.text }
