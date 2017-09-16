c5612106078168820eefbffb1b4423e4ec7d560b
MO <fengzilong1992@gmail.com>
2017-09-17 01:49:32 +0800

    style: convert tabs to spaces

--- scripts/devtools.config.js
+++ scripts/devtools.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     entry: './src/devtools/devtools.js',
-	copy: [
-		{
-			from: 'src/devtools/media',
-			to: 'static/media',
-		},
-	],
+    copy: [
+        {
+            from: 'src/devtools/media',
+            to: 'static/media',
+        },
+    ],
     webpack( config ) {
         config.output.publicPath = '/dist/';
         config.resolve.alias[ 'regularjs' ] = require.resolve( 'regularjs/dist/regular.js' );
